7f6ed5ff62c0a2cef6ab66770b367ab361b4e41a
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-23 09:26:47 +0900

    [level 1] Title: 내적, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,4 @@
 function solution(a, b) {
-    let sum = 0;
-    for(let i=0;i<a.length;i++){
-        sum += a[i]*b[i]
-    }
+    let sum = a.reduce((acc,cur,idx) => acc += cur*b[idx], 0)
     return sum;
 }
