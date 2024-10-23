2d8cae9f7024a1e23a01ca9554efd53903c2e6c9
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-23 09:34:52 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.06 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,16 +1,12 @@
-function isEven(num){
-    let count = 0;
-    for(let i=1;i<=num;i++){
-        if(num % i === 0) count++; 
-    }
-    console.log("cnt: ", count);
-    return count % 2 === 0 ? num : -1*num;
-}
-
 function solution(left, right) {
     let result = 0;
+    
     for(let i=left;i<=right;i++){
-        result += isEven(i);
+        let count = 0;
+        for(let j=1;j<=i;j++){
+            if(i % j === 0) count++;
+        }
+        result += count % 2 === 0 ? i : -1*i;
     }
     return result;
 }
