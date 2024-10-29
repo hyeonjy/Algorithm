b0a5da28006730e7d2f802c7eccee238996f9c54
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-29 09:46:50 +0900

    [level 1] Title: 예산, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    d.sort((a,b)=>a-b);
+    let total = 0;
+    let count = 0;
+    for(let i=0;i<d.length;i++){
+        if(total + d[i] > budget) break;
+        total += d[i];
+        count++;
+    }
+    return count;
+}
