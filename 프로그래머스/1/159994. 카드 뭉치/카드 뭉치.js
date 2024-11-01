7ef4e92e9e417f6b415d62e600d99d5fc372fe27
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-11-01 09:54:07 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,30 +1,12 @@
 function solution(cards1, cards2, goal) {
-  let checked = Array(goal.length).fill(false);
-
-  let idx = -1;
-  for (let index = 0; index < cards1.length; index++) {
-    const card1 = cards1[index];
-
-    if (goal.includes(card1) && idx < goal.indexOf(card1)) {
-      idx = goal.indexOf(card1);
-      checked[goal.indexOf(card1)] = true;
-    } else if (!goal.includes(card1)) {
-      break;
+  
+    let j = 0;
+    let k = 0;
+    
+    for(let i=0;i<goal.length;i++){
+        if(goal[i] === cards1[j]) j++;
+        else if(goal[i] === cards2[k]) k++;
+        else return "No"
     }
-  }
-
-  idx = -1;
-
-  for (let index = 0; index < cards2.length; index++) {
-    const card2 = cards2[index];
-
-    if (goal.includes(card2) && idx < goal.indexOf(card2)) {
-      idx = goal.indexOf(card2);
-      checked[goal.indexOf(card2)] = true;
-    } else if (!goal.includes(card2)) {
-      break;
-    }
-  }
-
-  return checked.every((value) => value === true) ? "Yes" : "No";
+    return "Yes";
 }
