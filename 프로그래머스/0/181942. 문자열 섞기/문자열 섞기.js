b2d1ef6d1da64997beec3a6ab46235a299ddb272
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-02-20 21:20:33 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,18 +1,8 @@
 function solution(str1, str2) {
-    const maxLength = Math.max(str1.length, str2.length);
-    let j = 0;
     let answer = "";
     
-    for(let i=0;i<maxLength;){
-        if(j === 0){
-            answer += str1[i];
-            j++;
-        }
-        else{
-             answer += str2[i];
-            i++;
-            j = 0;
-        }
+    for(let i=0;i<str1.length;i++){
+        answer += str1[i]+str2[i];
     }
     return answer;
 }
