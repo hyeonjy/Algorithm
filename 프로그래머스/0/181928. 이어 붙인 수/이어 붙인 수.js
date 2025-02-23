36b68623829bba17c03fe316da2c8f0bde468826
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-02-23 23:20:03 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+    let oddStr = "";
+    let evenStr = "";
+    
+    for(let i=0;i<num_list.length;i++){
+        if(num_list[i] % 2 === 0){
+            evenStr += num_list[i]
+        }
+        else{
+            oddStr += num_list[i]
+        }
+    }
+    
+    return Number(evenStr) + Number(oddStr);
+}
