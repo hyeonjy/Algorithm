e5014c849cf808bbd7a6a1d2b36819316337d677
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-02-23 22:55:27 +0900

    [level 0] Title: 코드 처리하기, Time: 9.73 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -2,21 +2,11 @@ function solution(code) {
     let mode = 0;
     let ret = "";
     for(let idx=0;idx<code.length;idx++){
-        if(mode === 0){
-            if(code[idx]!=="1" && idx % 2 === 0){
-                ret += code[idx];
-            }
-            else if(code[idx] === "1"){
-                mode = 1;
-            }
+        if(code[idx] !== "1" && idx % 2 === mode){
+                  ret += code[idx];
         }
-        else{
-            if(code[idx]!=="1" && idx % 2 !== 0){
-                ret += code[idx];
-            }
-            else if(code[idx] === "1"){
-                mode = 0;
-            }
+        else if(code[idx] === "1"){
+            mode = +!mode;
         }
     }
     
