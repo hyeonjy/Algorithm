0a32eae0bf3e6e8007b66ab99c1a8d499776c431
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-02-27 23:57:15 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.42 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 4.37 ms
+메모리: 36.7 MB, 시간: 5.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 23:55:34
+2025년 02월 27일 23:57:13
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,14 +1,6 @@
 function solution(arr, queries) {
-    let answer = [];
-    for(let i=0;i<queries.length;i++){
-        let min = Infinity;
-        for(let j=queries[i][0];j<=queries[i][1];j++){
-            if(arr[j] > queries[i][2] && min > arr[j]){
-                min = arr[j];
-            }
-        }
-        
-        answer.push(min === Infinity ? -1 : min);
-    }
-    return answer;
+    return queries.map(([s, e, k]) => {
+        const result = arr.slice(s, e + 1).filter(num => num > k);
+        return result.length > 0 ? Math.min(...result) : -1;
+    });
 }
