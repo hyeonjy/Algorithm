4d13dde321b923a518e6baa636e083b6c03fab87
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-03-21 23:09:00 +0900

    [level 0] Title: 접미사 배열, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-    let answer = [];
-    
-    for (let i = 0; i < my_string.length; i++) {
-        answer.push(my_string.slice(i));
-    }
-    
-    return answer.sort();
+    return Array.from(my_string, (_, i) => my_string.slice(i)).sort();
 }
