240b82869a3fe99c78678404a3834e50dc85793d
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2025-03-27 23:41:42 +0900

    [level 0] Title: 세로 읽기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,7 +1,3 @@
 function solution(my_string, m, c) {
-    let result = '';
-    for (let i = 0; i < my_string.length; i += m) {
-        result += my_string.slice(i, i + m)[c-1];
-    }
-    return result;
+    return [...my_string].filter((_,i) => i % m === c - 1).join("");
 }
